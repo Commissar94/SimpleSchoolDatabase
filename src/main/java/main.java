924b89c6375e829261f2f903298703850cd1490e
Commissar94 <andreykomissarov94@gmail.com>
2021-06-18 10:28:04 +0300

    some of the commented lines have been deleted

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -267,41 +267,6 @@ interface createLineInDB {
     }
 }
 
-//interface getHuman {
-//
-//    default Human getHumanById(String url, String user, String password, String sqlQuery, Human human) {
-//
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//            Connection connect = DriverManager.getConnection(url, user, password);
-//            PreparedStatement preparedStatement = connect.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
-//
-//            switch (human.getClass().getName()) {
-//                case "Teacher":
-//                    Teacher teacher = (Teacher) human;
-//                    preparedStatement.setString(1, teacher.name);
-//                    preparedStatement.setString(2, teacher.specialization);
-//                    preparedStatement.setString(3, teacher.schoolClass);
-//                    preparedStatement.execute();
-//                    return teacher;
-//                case "Pupil":
-//                    Pupil pupil = (Pupil) human;
-//                    preparedStatement.setString(1, pupil.name);
-//                    preparedStatement.setString(2, pupil.schoolClass);
-//                    preparedStatement.execute();
-//                    return pupil;
-//                default:
-//                    break;
-//            }
-//
-//
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return human;
-//
-//    }
-
 
 interface updateLineInDB {
 
